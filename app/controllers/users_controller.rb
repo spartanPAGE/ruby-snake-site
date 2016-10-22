ca426560c75ae26340a166bbaccf568bd20fb3f5
spartanPAGE <spartanpageagain@gmail.com>
2016-10-22 14:09:08 +0200

    fixed code styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
+    redirect_to :back, :alert => 'Access denied.' unless @user == current_user
   end
 
 end
